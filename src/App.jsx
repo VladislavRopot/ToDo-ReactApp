ce75e52caa2d5df6d68bddd9eb0a5bebe21cd857
Vladislav Ropot <vladislav.ropot.dev@gmail.com>
2025-01-02 13:22:27 +0200

    Split App component into InputText and TodoItems components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import './App.css';
 import TodoItems from './Components/TodoItems/TodoItems';
 import InputText from './Components/InputText/InputText';
+import { useState } from 'react';
 
 function App() {
+  const [todoItems, setTodoItems] = useState([]);
+
+  const addTodoItem = (item) => {
+    setTodoItems([...todoItems, item]);
+  };
+
+  const removeTodoItem = (index) => {
+    const confirmed = window.confirm('Are you sure?');
+    if (confirmed) {
+      const updatedItems = todoItems.filter((_, i) => i !== index);
+      setTodoItems(updatedItems);
+    }
+  };
 
   return (
     <>
-    <TodoItems />
-    
-    <InputText />
+      <InputText addTodoItem={addTodoItem} />
+      <TodoItems todoItems={todoItems} removeTodoItem={removeTodoItem} />
     </>
   );
 }
--- src/Components/InputText/InputText.jsx
+++ src/Components/InputText/InputText.jsx
@@ -1,25 +1,28 @@
 import { useState } from 'react';
 
-function InputText() {
-    const [inputValue, setInputValue] = useState('');
-    const [todoItems, setTodoItems] = useState([]);
+function InputText({ addTodoItem }) {
+  const [inputValue, setInputValue] = useState('');
 
-    const addTodoItem = ({ addTodoItem }) => {
-        setTodoItems([...todoItems, inputValue]);
-        setInputValue('');
-      };
+  const handleAdd = () => {
+    if (inputValue.trim()) {
+      addTodoItem(inputValue);
+      setInputValue('');
+    }
+  };
 
-    return (
-
-        <div>
-        <input type="text"
-          placeholder='Type here...'
-          value={inputValue}
-          onChange={(event) => setInputValue(event.target.value)} />
-
-        <button disabled={inputValue === ''} onClick={() => addTodoItem()}>Add</button>
-      </div>
-    );
-};
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Type here..."
+        value={inputValue}
+        onChange={(event) => setInputValue(event.target.value)}
+      />
+      <button disabled={!inputValue.trim()} onClick={handleAdd}>
+        Add
+      </button>
+    </div>
+  );
+}
 
 export default InputText;
